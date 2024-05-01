08239cd5335ec11339ea812a9003bc427dde123f
solucioneswebmarcos <marcossud185@gmail.com>
2024-05-01 20:36:25 +0200

    add files resme and jobs to the folder learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Engineer";
+        job1._starYear = 2001;
+        job1._endYear = 2003;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._starYear = 2004;
+        job2._endYear = 2014;
+
+        Resume resume = new Resume();
+        resume._name = "Marcos Arias";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
