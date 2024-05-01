1f324e4d3eb0dd7a2dd8bf6518ea910c827b5187
solucioneswebmarcos <marcossud185@gmail.com>
2024-05-01 17:55:28 +0200

    modified prep3 erase the example

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -29,29 +29,5 @@ class Program
         }
 
         Console.WriteLine("You guessed it!");
-
-        /* Random randomGenerator = new Random();
-         int magicNumber = randomGenerator.Next(1, 101);
-
-         int guess = -1;
-
-         while (guess != magicNumber)
-         {
-             Console.Write("What is your guess? ");
-             guess = int.Parse(Console.ReadLine());
-
-             if (magicNumber > guess)
-             {
-                 Console.WriteLine("Higher");
-             }
-             else if (magicNumber < guess)
-             {
-                 Console.WriteLine("Lower");
-             }
-             else
-             {
-                 Console.WriteLine("You guessed it!");
-             }
-         }*/
     }
 }
